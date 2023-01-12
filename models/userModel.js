fc93fb698173347b0429f1d3ed0df1092f962759
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-12 19:58:53 +0700

    userModel.js pre-save middleware to bcrypt password and set passwordConfirm to null or undefined

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -37,6 +38,22 @@ const userSchema = new mongoose.Schema({
       message: 'password is not matched',
     },
   },
+});
+
+// pre-save middleware to encrypt password before it saves to database
+userSchema.pre('save', async function (next) {
+  // if password field is not changed, then skip this middleware
+  if (!this.isModified('password')) return next();
+
+  // only run this middleware, if the password field is changed
+
+  // run the hash to bcrypt password field
+  this.password = await bcryptjs.hash(this.password, 12);
+
+  // delete the passwordConfirm field, because we don't want to save it into database
+  this.passwordConfirm = undefined;
+
+  next();
 });
 
 // initiate the model
