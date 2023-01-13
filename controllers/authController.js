4d5b5cb6080f549aa2386c6415ac4c028f003a80
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-13 20:58:26 +0700

    authController create jwt-token when user sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
@@ -8,6 +10,21 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
+
+  // jwt.sign(payload, secretOrPrivateKey, [options, callback])
+
+  const token = jwt.sign(
+    // create payload that contains all data that we wanna store into token
+    {
+      id: newUser._id,
+    },
+    // secret code for the token (use the JWT_SECRET from config.env)
+    process.env.JWT_SECRET,
+    // options : define when the token expired
+    {
+      expiredIn: process.env.JWT_EXPIRED_IN,
+    }
+  );
 
   res.status(201).json({
     status: 'success',
