1b62a507dfb4c836fbb16640396e1411db53c857
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-14 21:08:13 +0700

    authController create controller for checking user crendetial when login

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -32,5 +33,37 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1.) check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('please provide email and password!', 400));
+  }
+
+  // 2.) check if user exist && password is correct
+  const user = await User.findOne({ email: email }).select('+password');
+
+  // check the user crendetial
+  if (
+    // if there's not user
+    // or
+    !user ||
+    // call the instance method that return true/false to check if the request body password from client is correct/not
+    !(await user.correctPassword(password, user.password))
+  ) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  console.log(user);
+
+  // 3.) if everthing ok, send token to client
+  const token = '';
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
