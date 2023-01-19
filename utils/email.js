c3244ea07c21f4e0f47edd2e7970668f361a793d
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-19 09:15:54 +0700

    email.js setup configuration to send email to user

---
+++ utils/email.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (option) => {
+  // 1.) create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // 2.) define email option
+  const mailOptions = {
+    from: 'Hubla <hello@hubla.io>',
+    // configure the recipient information that we got from option argument
+    to: option.email,
+    subject: option.subject,
+    text: option.message,
+    // html:
+  };
+
+  // 3.) actually send the email
+  await transporter.sendMail(mailOptions);
+};
