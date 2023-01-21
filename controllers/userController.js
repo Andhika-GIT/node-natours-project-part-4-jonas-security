bce70095c8ec332ed0a506263a67944f20e57cfd
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-21 16:04:21 +0700

    userController create new handler for updating user data create a function that filtering req objects that are not allowed to be updated to database

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,55 @@
 ////// * SEPARATE HANDLER FOR DIFFERENT REQUEST * /////
 
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const User = require('../models/userModel');
+
+//  functions that will take care of filtering the object
+const filterObj = (obj, ...allowedFields) => {
+  let newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
 exports.getAllUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'this route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1.) Create error if user trying to update password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'this route is not for password updates. Please use update my password feature',
+        400
+      )
+    );
+  }
+
+  // 2.) filtered out unwanted fields from request that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3.) update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
