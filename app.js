e9ae5bcc6591a8861df323dcd9debbc1dbda0f01
Andhika Prasetya <andhikapr081@gmail.com>
2023-01-22 15:49:59 +0700

    app implement header package and refactoring our global middleware position

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 // import error handling class file
 const AppError = require('./utils/appError');
@@ -14,18 +15,8 @@ const globalErrorController = require('./controllers/errorController');
 
 const app = express();
 
-// parses incoming JSON requests and puts the parsed data in req.body
-app.use(express.json());
-
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
-
-app.use((req, res, next) => {
-  // for checking headers
-  // console.log(req.headers);
-  next();
-});
+// set security HTTP headers (helmet)
+app.use(helmet());
 
 // limit incoming request from same ip adress
 const limiter = rateLimit({
@@ -36,15 +27,20 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
+// parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json({ limit: '50kb' }));
+
+// use morgan when in development mode
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 // create middleware route definition
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // will be caught in global error middleware
-  // const err = new Error(`can't find the ${req.originalUrl} on this server!`);
-  // err.status = 'fail';
-  // err.statusCode = 404;
+  // this function will run when there's request url route that is not defined in the middleware route definition
 
   // pass the error to global error middleware using next()
   next(new AppError(`can't find the ${req.originalUrl} on this server!`, 404));
